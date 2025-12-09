5365b700f798fc18f663cc07c0530fb524dfb2d4
Luiz Fernando de Lira <luizflira@gmail.com>
2025-12-09 11:41:34 -0300

    [feature] Added custom drawer for ScriptableVariables and GameEvents

---
+++ Assets/GameEventSystemPackage/Editor/ScriptableObjectDrawer/ScriptableObjectDrawer.cs
@@ -0,0 +1,42 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using cpluiz.GameEventSystem;
+
+[CustomPropertyDrawer(typeof(GameEvent), true)][CustomPropertyDrawer(typeof(ScriptableVariable), true)]
+public class ScriptableObjectDrawer : PropertyDrawer
+{
+    // Cached scriptable object editor
+    private Editor editor = null;
+
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        // Draw label
+        EditorGUI.PropertyField(position, property, label, true);
+     
+        // Draw foldout arrow
+        if (property.objectReferenceValue != null)
+        {
+            property.isExpanded = EditorGUI.Foldout(position, property.isExpanded, GUIContent.none);
+        }
+        else
+        {
+            property.isExpanded = false;
+        }
+
+        // Draw foldout properties
+        if (property.isExpanded)
+        {
+            // Make child fields be indented
+            EditorGUI.indentLevel++;
+         
+            // Draw object properties
+            if (!editor)
+                Editor.CreateCachedEditor(property.objectReferenceValue, null, ref editor);
+            editor.OnInspectorGUI();
+         
+            // Set indent back to what it was
+            EditorGUI.indentLevel--;
+        }
+    }
+}
